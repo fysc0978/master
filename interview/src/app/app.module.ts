8418b3217d6c182caaf1b4e6f9969202f4404e18
Fredy Suarez <40718721+fysc0978@users.noreply.github.com>
2020-07-17 19:20:59 -0500

    fix4 (#4)

--- interview/src/app/app.module.ts
+++ interview/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressComponent } from './address/address.component';
 import { HomeComponent } from './home/home.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { EmailComponent } from './email/email.component';
@@ -11,6 +12,7 @@ import { EmailComponent } from './email/email.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AddressComponent,
     HomeComponent,
     ContactsComponent,
     EmailComponent
